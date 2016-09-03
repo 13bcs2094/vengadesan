c9686b5b055e8831428b3161d86ad1a982caa984
13bcs2094 <vengadesan1996@gmail.com>
2016-09-03 09:09:27 +0530

    Update and rename longest.java to panagram.java

---
+++ panagram.java
@@ -0,0 +1,35 @@
+package pro;
+
+import java.util.Scanner;
+
+public class panagram 
+{
+	static Scanner in=new Scanner(System.in);
+public static void main(String[] args) 
+{
+	
+	String s=in.nextLine();
+	int a=s.length();
+	String s1="abcdefghijklmnopqrstuvwxyz";
+	int b=s1.length();
+	int temp=0;
+	for(int i=0;i<a;i++)
+	{
+		for(int j=0;j<b;j++)
+		{
+			if(s.charAt(i)==s1.charAt(j))
+			{
+				temp++;
+			}
+		}
+	}
+		if(temp==a)
+		{
+			System.out.println("It is a panagram");
+		}
+		else
+		{
+			System.out.println("It is not a panagram");
+		}
+}
+}
