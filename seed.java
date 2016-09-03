9616209d7afce6b3d7aceac445b9c4b1ece74191
13bcs2094 <vengadesan1996@gmail.com>
2016-09-03 09:20:13 +0530

    Update and rename decrease.java to seed.java

--- seed.java
+++ seed.java
@@ -2,7 +2,7 @@ package pro;
 
 import java.util.Scanner;
 
-public class decrease {
+public class seed{
         public static void main(String[] args) 
         {
 			Scanner sc=new Scanner(System.in);
