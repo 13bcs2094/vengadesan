d9896b3c50f4ecb5f822afa34134a80f90c94787
13bcs2094 <vengadesan1996@gmail.com>
2016-09-06 09:47:00 +0530

    Update repeat.java

--- repeat.java
+++ repeat.java
@@ -1,42 +1,33 @@
-package hunter;
+
+package my_program;
 
 import java.util.Scanner;
 
-public class repeat {
-                       
-	private static Scanner sc;
+public class My_program
+{
 
-	public static void main(String[] args) 
-	{
-		sc = new Scanner(System.in);
-		int a[]=new int[10];
-		for (int i = 0; i < a.length; i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		//System.out.println(b.length);
-		int c[]=new int[10];
-		int temp=0;
-		for (int i = 0; i < a.length; i++) 
-		{
-			for (int j = i+1; j < a.length; j++) 
-			{
-				if(a[i]==a[j])
-				{
-					int h=temp+1;
-					c[i]=h;
-					//System.out.println(temp+" "+a[]);
-				}
-			}
-			
-			//System.out.println(c[i]);
-		}
-		
-		for (int i = 0; i < c.length; i++)
-		{
-			System.out.println(c[i]);
-			
-		}
-	}
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        int a[]=new int[5];
+        
+        for (int i = 0; i < 5; i++)
+        {
+           a[i]=sc.nextInt();
+        }
+        for (int i = 0; i < a.length; i++)
+        {
+            for (int j = 1; j < a.length; j++) 
+            {
+                int n=a[i]+a[j];
+              if(k==n)  
+              {
+                  System.err.println("k");
+              }
+            }
+        }
+       
+    }
+    
 }
-//1 2 3 4 5 6 7 8 9 1
