f9248dbe1954f7e6e2a1667adc234803110aa0ac
13bcs2094 <vengadesan1996@gmail.com>
2016-09-09 10:12:59 +0530

    Update and rename repeat.java to primenumber.java

---
+++ primenumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Primenumber {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int temp=0;
+		for(int i=2;i<r;i++)
+		{
+			int temp1=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+					temp1=1;
+			}
+			if(f==0)
+			{
+				//System.out.print(i+" ");
+				temp=temp+i;
+			}
+		}
+		System.out.println("temp= "+temp);
+	}
+}
